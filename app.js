25ea174259f06af0a500935cfd19c59103386071
Yasir Gaji <gajiyasir@gmail.com>
2023-12-16 06:20:26 +0100

    rendering elements dynamically through javascript

--- app.js
+++ app.js
@@ -35,5 +35,10 @@ fetch(json_url).then(Response => Response.json())
     `
 
     cards.appendChild(card);
-  })
+  });
+
+  document.getElementById('title').innerText = data[0].name;
+  document.getElementById('gen').innerText = data[0].genre;
+  document.getElementById('date').innerText = data[0].date;
+  document.getElementById('rate').innerHTML = `<span>IMDB </span> <i class="bi bi-star-fill"></i> ${data[0].imdb}`
 })
